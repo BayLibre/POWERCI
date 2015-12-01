6b2aae6d0e75cffddeaa880a653b2babe5e0507f
Marc Titinger <marc.titinger@baylibre.com>
2015-12-01 15:32:04 +0100

    update DUT list
    Signed-off-by: Marc Titinger <marc.titinger@baylibre.com>

--- scripts/lab-setup/add-boards-baylibre.sh
+++ scripts/lab-setup/add-boards-baylibre.sh
@@ -4,9 +4,9 @@ root_dir=/home/powerci/POWERCI
 mkdir -p $root_dir/fs-overlay/etc/lava-dispatcher/devices
 
 sudo /usr/share/lava-server/add_device.py kvm kvm01
-sudo /usr/share/lava-server/add_device.py beaglebone-black dut0-bbb -t 2000 -p 100 -b
-sudo /usr/share/lava-server/add_device.py beaglebone-black dut1-bbb -t 2001 -p 101
-sudo /usr/share/lava-server/add_device.py juno dut2-juno -t 2010 -p 110
+sudo /usr/share/lava-server/add_device.py beaglebone-black dut0-bbb -t 2000  -b
+sudo /usr/share/lava-server/add_device.py panda-es dut1-panda-es -t 2001 
+sudo /usr/share/lava-server/add_device.py juno dut2-juno -t 2002 
 
 sudo service lava-server restart
 sudo service apache2 restart
