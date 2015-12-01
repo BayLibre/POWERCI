f065b137f6a4f5b3715726ea1befd298df4225fc
Marc Titinger <marc.titinger@baylibre.com>
2015-12-01 17:55:11 +0100

    update jobs and add token files
    Signed-off-by: Marc Titinger <marc.titinger@baylibre.com>

--- scripts/user/0_auth-add.sh
+++ scripts/user/0_auth-add.sh
@@ -6,5 +6,5 @@ echo "make sure to log into $LAVA_SERVER_IP, got to API and request a token"
 echo "make sure to properly tune lava-env.inc for your user."
 echo "In peculiar, the LAVA_RPC_LOGIN path and protocol must be consistent"
 
-lava-tool auth-add $LAVA_RPC_LOGIN
+lava-tool auth-add --token-file token-files/$USER.tok $LAVA_RPC_LOGIN
 
---
+++ scripts/user/token-files/README.md
@@ -0,0 +1,7 @@
+# Token File Folder #
+
+This folder contains each user's token file
+in the form ${USER}.tok
+
+this will be used with the `lava-tools add-auth` step.
+
