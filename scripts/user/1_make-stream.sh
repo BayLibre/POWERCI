fdbce931ef5ed130ab3c5999d9d066820ce1154d
Marc Titinger <marc.titinger@baylibre.com>
2015-12-22 12:39:26 +0100

    fix bundle creation stance
    Signed-off-by: Marc Titinger <marc.titinger@baylibre.com>

--- scripts/user/1_make-stream.sh
+++ scripts/user/1_make-stream.sh
@@ -2,4 +2,4 @@
 
 . ./lava-env.inc
 
-lava-tool make-stream --dashboard-url $LAVA_HTTP://$LAVA_SERVER_IP/RPC2/ $MY_BUNDLE_STREAM
+lava-tool make-stream --dashboard-url $LAVA_RPC_LOGIN $MY_BUNDLE_STREAM
