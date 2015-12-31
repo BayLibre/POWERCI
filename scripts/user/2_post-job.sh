965dc4a8f434715f44263f6d61b25a37fc3c141f
Powerci Testuser <lab-admin@baylibre.com>
2015-12-31 16:11:09 +0100

    scripts: change default for wan/ssl client
    Signed-off-by: Powerci Testuser <lab-admin@baylibre.com>

--- scripts/user/2_post-job.sh
+++ scripts/user/2_post-job.sh
@@ -2,4 +2,8 @@
 
 . ./lava-env.inc
 
-lava-tool submit-job $LAVA_RPC_LOGIN $1
+cat $1 | sed 's#BUNDLE_STREAM#'"$BUNDLE_STREAM"'#' >  /tmp/last.json
+sed 's#LAVA_SERVER_IP#'"$LAVA_SERVER_IP"'#' -i  /tmp/last.json
+sed 's#LAVA_SERVER#'"$LAVA_SERVER_IP"'#' -i  /tmp/last.json
+
+lava-tool submit-job $LAVA_RPC_LOGIN /tmp/last.json
