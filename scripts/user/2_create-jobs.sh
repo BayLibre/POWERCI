cdd940492a17182377b7fe4fa1906b48749c5cec
Marc Titinger <mtitinger@baylibre.com>
2016-01-06 10:39:50 +0100

    wip: 2_create-jobs.sh trials
    Signed-off-by: Marc Titinger <mtitinger@baylibre.com>

--- scripts/user/2_create-jobs.sh
+++ scripts/user/2_create-jobs.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-python lava-ci/lava-kernel-ci-job-creator.py http://storage.kernelci.org/next/next-20160104/ --plans boot --targets juno --arch arm64
+
+## create jobs for JUNO
+#python lava-ci/lava-kernel-ci-job-creator.py http://storage.kernelci.org/next/next-20160104/ --plans boot --targets juno --arch arm64
+
+## create jobs for BBB
+python lava-ci/lava-kernel-ci-job-creator.py http://storage.kernelci.org/next/next-20160104/ --plans boot --targets beaglebone-black --arch arm
+
+## create jobs for Panda-es
+python lava-ci/lava-kernel-ci-job-creator.py http://storage.kernelci.org/next/next-20160104/ --plans boot --targets panda-es --arch arm
