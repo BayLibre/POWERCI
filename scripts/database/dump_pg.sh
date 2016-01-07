f43b46e230ba028b8dea97d0d0d5a4c4e68fa2d2
Marc Titinger <mtitinger@baylibre.com>
2016-01-07 11:59:23 +0100

    remove db migration scripts for now
    Signed-off-by: Marc Titinger <mtitinger@baylibre.com>

--- scripts/database/dump_pg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-pg_dump \
-     --no-owner \
-     --format=custom \
-     --host=$dbserver \
-     --port=$dbport \
-     --username=$dbuser \
-     --no-password $dbname \
-     --schema=public \
