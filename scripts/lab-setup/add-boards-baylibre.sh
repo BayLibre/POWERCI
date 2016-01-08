f1083f18400e7ce7648d77a37e6464d7ef571441
Marc Titinger <mtitinger@baylibre.com>
2016-01-08 12:11:46 +0100

    replace juno with jetson-tk1 in config script
    Signed-off-by: Marc Titinger <mtitinger@baylibre.com>

--- scripts/lab-setup/add-boards-baylibre.sh
+++ scripts/lab-setup/add-boards-baylibre.sh
@@ -10,7 +10,8 @@ sudo ./add_baylibre_device.py qemu-arm qemu0
 sudo ./add_baylibre_device.py kvm kvm01
 sudo ./add_baylibre_device.py beaglebone-black dut0-bbb -t 2000 -p 1  -b
 sudo ./add_baylibre_device.py panda-es dut1-panda-es -t 2001 -p 2
-sudo ./add_baylibre_device.py juno dut2-juno -t 2002 -p 3
+#sudo ./add_baylibre_device.py juno dut2-juno -t 2002 -p 3
+sudo ./add_baylibre_device.py jetson-tk1 dut2-jetson-tk1 -t 2002 -p 3
 
 sudo service lava-server restart
 sudo service apache2 restart
